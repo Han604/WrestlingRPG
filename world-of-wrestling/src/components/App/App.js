645d39e594be50a0f2bed55bd50c24f7fc37f828
Eric Han <eric_h94@hotmail.com>
2020-05-20 19:32:25 -0400

    eric test grid

--- world-of-wrestling/src/components/App/App.js
+++ world-of-wrestling/src/components/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Game from '../Game';
 import styled from 'styled-components';
+import EricTest from '../Game/EricTest/EricTest';
+
 
 function App() {
   return (
     <AppBody className='App'>
       <h1>World of Wrestling!!</h1>
+      <EricTest/>
       <Game />
     </AppBody>
   );
